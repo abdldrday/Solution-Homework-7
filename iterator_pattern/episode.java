913c4b0f647c7e6adf0c3d438e4d78e6db658c97
abdldrday <abdildirday14@gmail.com>
2025-04-30 15:08:02 +0500

    first commit

---
+++ iterator_pattern/episode.java
@@ -0,0 +1,16 @@
+package iterator_pattern;
+
+class Episode {
+    private String title;
+    private int runtimeSec;
+
+    public Episode(String title, int runtimeSec){
+        this.title = title;
+        this.runtimeSec = runtimeSec;
+    }
+
+    public String getTitle() { return title; }
+    public int getRuntimeSec() { return runtimeSec; }
+}
+
+
