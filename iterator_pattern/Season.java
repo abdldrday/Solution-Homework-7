1d09b6538f8612f2523849d5a20cbe36e44b17eb
abdldrday <abdildirday14@gmail.com>
2025-04-30 18:57:56 +0500

    season class

--- iterator_pattern/Season.java
+++ iterator_pattern/Season.java
@@ -1,2 +1,25 @@
-public class Season {
+class Season implements Iterable<Episode>{
+    private List<Episode> episodes = new ArrayList<>();
+
+
+    public void addEpisode(Episode e) {
+        episodes.add(e);
+    }
+
+    public EpisodeIterator getNormalIterator() {
+        return new SeasonIterator(episodes);
+    }
+
+    public EpisodeIterator getReverseIterator() {
+        return new ReverseSeasonIterator(episodes);
+    }
+
+    public EpisodeIterator getShuffleIterator(long seed) {
+        return new ShuffleSeasonIterator(episodes, seed);
+    }
+
+    public Iterator<Episode> iterator() {
+        return new SeasonIterator(episodes);
+    }
 }
+
