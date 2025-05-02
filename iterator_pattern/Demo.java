ac0ca6f043732bce3e931903feac01b909803b80
abdldrday <abdildirday14@gmail.com>
2025-05-02 22:24:43 +0500

    demo class

--- iterator_pattern/Demo.java
+++ iterator_pattern/Demo.java
@@ -1,5 +1,33 @@
 public class Demo {
     public static void main(String[] args){
 
+        Season s1 = new Season();
+        s1.addEpisode(new Episode("S1E1", 1000));
+        s1.addEpisode(new Episode("S1E2", 1100));
+
+        Season s2 = new Season();
+        s2.addEpisode(new Episode("S2E1", 1200));
+        s2.addEpisode(new Episode("S2E2", 1300));
+
+        Series series = new Series();
+        series.addSeason(s1);
+        series.addSeason(s2);
+
+        System.out.println("Normal:");
+        EpisodeIterator it1 = s1.getNormalIterator();
+        while (it1.hasNext()) System.out.println(it1.next());
+
+        System.out.println("\nReverse:");
+        EpisodeIterator it2 = s1.getReverseIterator();
+        while (it2.hasNext()) System.out.println(it2.next());
+
+        System.out.println("\nShuffle:");
+        EpisodeIterator it3 = s1.getShuffleIterator(42);
+        while (it3.hasNext()) System.out.println(it3.next());
+
+        System.out.println("\nBinge watch:");
+        EpisodeIterator it4 = series.getBingeIterator();
+        while (it4.hasNext()) System.out.println(it4.next());
+
     }
 }
--- iterator_pattern/Series.java
+++ iterator_pattern/Series.java
@@ -10,3 +10,25 @@ public class Series {
     }
 
 }
+
+class BingeIterator implements EpisodeIterator {
+    private Queue<Episode> allEpisodes = new LinkedList<>();
+
+    public BingeIterator(List<Season> seasons) {
+        for (Season season : seasons) {
+            for (Episode e : season) {
+                allEpisodes.add(e);
+            }
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !allEpisodes.isEmpty();
+    }
+
+    @Override
+    public Episode next() {
+        return allEpisodes.poll();
+    }
+}
